ff4e770e94cb915fd10791590a4f7aeca3f6ae2b
nalostta <shah.manthan38@gmail.com>
2019-02-26 23:51:49 +0530

    got the total design to work including : sending data in a valid format and reading the same, issues : some clock cycles are eaten out despite removing all the latches

--- bidir_wrapper/bidir_wrapper.v
+++ bidir_wrapper/bidir_wrapper.v
@@ -30,9 +30,9 @@ input  my_state_in,clk,Locked;
 output my_state_out,partner_state_out;
 inout data_link;
 wire data_read;
-wire[2:0] count_next;
-reg my_state_out,data_en,partner_state_out,count_en;
-reg[1:0] shift,count;
+wire[2:0] count_next,rcount_next;
+reg my_state_out,data_en,partner_state_out,read_en,rshift;
+reg[1:0] shift,count,rcount;
 
 //check if the line goes low, yes: allot 2 cycles to read data
 //else check self state 
@@ -47,11 +47,12 @@ io's:
 assign data_read	=	data_link;
 assign data_link	=	data_en?	my_state_out:1'bz;
 assign count_next	=	count+1'b1;
+assign rcount_next	=	rcount+1'b1;
 
 always @(posedge clk or posedge Locked)begin
 	if(Locked)begin
 		//algorithm
-			if(my_state_in!=my_state_out && data_en!=1'b1)begin
+			if(my_state_in!=my_state_out && data_en!=1'b1 && read_en!=1'b1)begin
 				shift[1]<=my_state_in;
 				data_en<=1'b1;
 			end
@@ -62,8 +63,9 @@ always @(posedge clk or posedge Locked)begin
 		end else begin
 		//reset state
 		data_en<=1'b0;
+		shift[1]<=1'b0;
 		//my_state_out<=1'b0;
-		partner_state_out<=1'b0;
+		//partner_state_out<=1'b0;
 	end
 end
 
@@ -76,6 +78,33 @@ always @(posedge clk or posedge data_en)begin
 		count<=2'b0;
 		shift[0]<=1'b0;
 		my_state_out<=my_state_out && Locked;
+	end
+end
+
+always @(posedge clk or posedge Locked)begin
+	if(Locked)begin
+		if(data_read==1'b0 && data_en!=1)begin
+			read_en<=1;
+		end
+		//
+		if(rcount==2'b10)begin
+			
+			read_en<=0;
+		end
+	end else begin
+		read_en<=1'b0;
+	end
+end
+
+always @(posedge clk or posedge read_en)begin
+	if(read_en)begin
+		rcount<=rcount_next;
+		rshift<=data_read;
+		partner_state_out<=rshift;
+	end else begin
+		rshift<=1'b0;
+		rcount<=2'b0;
+		partner_state_out<=partner_state_out && Locked;
 	end
 end
 endmodule
