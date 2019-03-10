12ddc4d4c3519b35bd32758f4a816cd8f7a6c971
nalostta <shah.manthan38@gmail.com>
2019-03-11 00:14:06 +0530

    Made edits bidir_wrapper2,signals seem valid in the simulation now

--- bidir_wrapper/bidir_wrapper2.v
+++ bidir_wrapper/bidir_wrapper2.v
@@ -53,11 +53,12 @@ inout data_link;
 //resources
 wire data_read;
 
-reg read_en,ReadCounter_en,WriteCounter_en,my_state_out,partner_state_out,write_en,Rbuffer;
-reg[1:0] state,Wcount,Rcount,Wbuffer;
+reg read_en,my_state_out,partner_state_out,write_en,Rbuffer;
+reg[1:0] state,Wcount,Rcount;
+reg[2:0] Wbuffer;
 
 assign data_read	=	data_link;
-assign data_link	=	(write_en)?	Wbuffer[1]:1'bz;
+assign data_link	=	(write_en)?	Wbuffer[2]:1'bz;
 
 //control module:
 always @(posedge clk or posedge Locked)begin
@@ -65,76 +66,67 @@ always @(posedge clk or posedge Locked)begin
 		case(state)
 		
 			2'b0	: begin	//Line Idle check for read or write cycle
-				if(data_link==1'b0)	state <= 2'b01;
-				else if(my_state_in!=my_state_out)begin
-					state		<=	2'b10;
-					write_en	<=1'b1;
-					my_state_out	<=my_state_in;
-				end else begin
-					write_en<=1'b0;
+				if(data_link==1'b0)	state = 2'b01;
+				else if(my_state_in!=my_state_out)	state		=	2'b10;
 				end
-			end
 			
 			
 			2'b01	:	begin	//module in "read" cycle 
 				//check if already reading?
-				if(ReadCounter_en==1'b1)begin
-					if(Rcount==2'b01)begin	//if counter has counted to 2 cycles, end state and get data
-						ReadCounter_en	<=	1'b0;
-						state	<=	2'b0;
-						partner_state_out <= Rbuffer;
-					end
+				if((read_en==1'b1)&&(Rcount==2'b01))begin //if counter has counted to 2 cycles, end state and get data
+					read_en				=	1'b0;
+					state					=	2'b0;
+					partner_state_out = Rbuffer;
 				end else begin
-					ReadCounter_en	<=	1'b1;
+					read_en				=	1'b1;
 				end
 			end
 			
 			
 			2'b10	: begin	//module in "write" cycle
-				if(WriteCounter_en==1'b1)begin
-					if(Wcount==2'b01)begin
-						WriteCounter_en<=1'b0;
-						state	<=	2'b0;
-						write_en<=1'b0;
-					end 
+				if((write_en==1'b1) && (Wcount==2'b10))begin
+					state					=	2'b0;
+					write_en				=	1'b0;
 				end else begin
-					WriteCounter_en	<=	1'b1;
-					Wbuffer[0]<=my_state_in;
+					my_state_out		=	my_state_in;
+					Wbuffer[0]			=	my_state_in;
+					write_en				=	1'b1;
 				end
 			end
 			//
-			default :	state <= 2'b0;
+			default :	state 		= 2'b0;
 		endcase
 		
 	end else begin
-		//state <= 2'b0;
-		Wbuffer[0]<=1'b0;
-		//write_en<=1'b0;
-		my_state_out<=1'b1;
-		partner_state_out<=1'b1;
+	write_en=1'b0;
+		Wbuffer[0]			=	1'b0;
+		my_state_out		=	1'b0;
+		partner_state_out	=	1'b0;
+		read_en				=	1'b0;
 	end
 end
 
 
 //Read module
-always @(posedge clk or posedge ReadCounter_en)begin
-	if(ReadCounter_en)begin
-		Rcount<=Rcount+1'b1;
-		Rbuffer<=data_read;
-	end else begin
-		Rbuffer<=1'b0;
-		Rcount<=2'b0;
-	end
+always @(posedge clk or posedge read_en)begin
+		if(read_en)begin
+			Rcount	=	Rcount+1'b1;
+			Rbuffer	=	data_read;
+		end else begin
+			Rbuffer	=	1'b0;
+			Rcount	=	2'b0;
+		end
 end
 
 //Write module
-always @(posedge clk or posedge WriteCounter_en)begin
-	if(WriteCounter_en==1'b1)begin
-		Wcount<=Wcount+1'b1;
-		Wbuffer[1]<=Wbuffer[0];
+always @(posedge clk or posedge write_en)begin
+	if(write_en==1'b1)begin
+		Wcount		=	Wcount+1'b1;
+		Wbuffer[2]	=	Wbuffer[1];
+		Wbuffer[1]	=	Wbuffer[0];
 	end else begin
-		Wcount<=2'b0;
-		Wbuffer[1]<=1'b0;
+		Wcount		=	2'b0;
+		Wbuffer[2:1]	=	2'b0;
 	end
 end 
 endmodule
